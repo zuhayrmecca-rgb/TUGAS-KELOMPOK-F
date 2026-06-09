d9436fc2bebf59137ad814d8681a439c53aecbd1
zuhayrmecca-rgb <zuhayr.mecca@mhs.unsoed.ac.id>
2026-06-09 19:35:22 +0700

    Update |   |-- 02_string_kalimat.py

--- |-- src_python/|   |-- 02_string_kalimat.py
+++ |-- src_python/|   |-- 02_string_kalimat.py
@@ -1,1 +1,11 @@
+#Membuat objek string K1 sampai K4 dengan tanda baca lengkap
+K1 = "Saya tak 'kan menyerah."
+K2 = 'Ia berkata, "Aku menyayangimu."'
+K3 = '"Coba jelaskan pengertian \'cross-validation\' dalam Machine Learning!"'
+K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
 
+#Mencetak string ke layar sesuai instruksi soal
+print(K1)
+print(K2)
+print(K3)
+print(K4)
