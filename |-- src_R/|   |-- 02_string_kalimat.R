4b9b8d4125b6c1971faf1961a3d4620f10867215
zuhayrmecca-rgb <zuhayr.mecca@mhs.unsoed.ac.id>
2026-06-09 15:28:28 +0700

    Update |   |-- 02_string_kalimat.R

--- |-- src_R/|   |-- 02_string_kalimat.R
+++ |-- src_R/|   |-- 02_string_kalimat.R
@@ -1,13 +1,26 @@
-#PROGRAM 2
-# Membuat objek string K1 sampai K4
-K1 <- " Saya tak ‘kan menyerah."
-K2 <- " Ia berkata, “Aku menyayangimu.”"
-K3 <- " Coba jelaskan pengertian ‘cross-validation’ dalam Machine Learning!"
-K4 <- " Surat keputusan itu bernomor 62/UN.34/19/2023."
+# PROGRAM 2: STRING KALIMAT
+# FUNGSI
+cetak <- function(nama, teks) {
+  if (teks == "" || grepl("\\$|\\^|\\=|\\*|`|~", teks)) {
+    cat(nama, ": ", teks, " - TIDAK VALID\n", sep="")
+  } else {
+    cat(nama, ": ", teks, " - VALID\n", sep="")
+  }
+}
+# SKENARIO 1 
+cetak("K1", "Saya tak 'kan menyerah.")
+cetak("K2", "Ia berkata,'Aku menyayangimu.'")
+cetak("K3", "Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!")
+cetak("K4", "Surat keputusan itu bernomor 62/UN.34/19/2023.")
 
-# Catatan: Fungsi print() di R secara bawaan akan menampilkan nomor indeks [1] dan tanda petik.
-# Jika ingin hasilnya bersih persis seperti di gambar, gunakan fungsi cat() dengan baris baru (\n):
-cat(K1, "\n")
-cat(K2, "\n")
-cat(K3, "\n")
-cat(K4, "\n")
+# SKENARIO 2 
+cetak("K1", "Wahai para insan pengembara, janganlah kau ragu tuk melangkah.")
+cetak("K2", "Dia berbisik,'Rahasia alam semesta tersimpan dalam hati yang sunyi.'")
+cetak("K3", "Di balik kabut 'senja' ada sejuta mimpi yang belum tertidur.")
+cetak("K4", "Puisi itu lahir dari jiwa yang luka: 22/04/1999 sebagai saksi bisu.")
+
+# SKENARIO 3 
+cetak("K1", "Harga barang itu $100.")
+cetak("K2", "Hasilnya 5^2 = 25.")
+cetak("K3", "Rumus luas persegi adalah s*s.")
+cetak("K4", "Dia berkata *halo* semua.")
