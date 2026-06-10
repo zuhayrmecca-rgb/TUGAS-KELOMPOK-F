a2db1569318d19f43fd7031af66b93408a20dc27
zuhayrmecca-rgb <zuhayr.mecca@mhs.unsoed.ac.id>
2026-06-10 11:58:16 +0700

    Update |   |-- 02_string_kalimat.R

--- |-- src_R/|   |-- 02_string_kalimat.R
+++ |-- src_R/|   |-- 02_string_kalimat.R
@@ -1,8 +1,8 @@
 #PROGRAM 2
 # Membuat objek string K1 sampai K4
-K1 <- " Saya tak ‘kan menyerah."
-K2 <- " Ia berkata, “Aku menyayangimu.”"
-K3 <- " Coba jelaskan pengertian ‘cross-validation’ dalam Machine Learning!"
+K1 <- " Saya tak 'kan menyerah."
+K2 <- " Ia berkata, \"Aku menyayangimu.\""
+K3 <- " Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!"
 K4 <- " Surat keputusan itu bernomor 62/UN.34/19/2023."
 
 # Catatan: Fungsi print() di R secara bawaan akan menampilkan nomor indeks [1] dan tanda petik.
