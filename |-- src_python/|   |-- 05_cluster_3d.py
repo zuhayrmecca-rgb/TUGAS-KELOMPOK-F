8adc14decc7a77eec06d1c7d70b1ac0ff3162213
zuhayrmecca-rgb <zuhayr.mecca@mhs.unsoed.ac.id>
2026-06-18 09:50:50 +0700

    Update |   |-- 05_cluster_3d.py

--- |-- src_python/|   |-- 05_cluster_3d.py
+++ |-- src_python/|   |-- 05_cluster_3d.py
@@ -7,21 +7,21 @@ def cluster(x, y, z):
     if not isinstance(x, (int, float)) or not isinstance(y, (int, float)) or not isinstance(z, (int, float)):
         print("STATUS: TIDAK VALID")
         return()
-    
+
     #Pusat cluster
     A = (2, 1, 3)
     B = (1, -4, 6)
     C = (-2, 3, -2)
-    
+
     #Hitung jarak Euclidean ke masing-masing cluster
-    jA = math.sqrt((x - 2)**2 + (y - 1)**2 + (z - 3)**2)
-    jB = math.sqrt((x - 1)**2 + (y + 4)**2 + (z - 6)**2)
-    jC = math.sqrt((x + 2)**2 + (y - 3)**2 + (z + 2)**2)
-    
+    jA = math.sqrt((x - A [0])**2 + (y - A [1])**2 + (z - A [2])**2)
+    jB = math.sqrt((x - B [0])**2 + (y + B [1])**2 + (z - B [2])**2)
+    jC = math.sqrt((x + C [0])**2 + (y - C [1])**2 + (z + C [2])**2)
+
     print(f"Jarak ke A: {jA:.3f}")
     print(f"Jarak ke B: {jB:.3f}")
     print(f"Jarak ke C: {jC:.3f}")
-    
+
     #Tentukan cluster terdekat
     if jA <= jB and jA <= jC:
         print("STATUS: VALID - CLUSTER A")
@@ -43,6 +43,6 @@ cluster(1, -4, 6)
 print("SKENARIO 3")
 cluster("a", "b", "c")
 
-#SKENARIO 4 (Titik U: 1, b, 5)
-print ("SKENARIO 4")
-cluster(1, "b", 5)
+#SKENARIO 4 (Titik U: a, b, c)
+print("SKENARIO 4")
+cluster("1", "b", "5")
