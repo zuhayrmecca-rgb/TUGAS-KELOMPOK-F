8704cdcbcacb0b198e9e26e6c399c67b3e7250b3
putrisitanggang-rgb <putri.sitanggang@mhs.unsoed.ac.id>
2026-06-25 16:44:10 +0700

    Update |   |-- 02_string_kalimat.R

--- |-- src_R/|   |-- 02_string_kalimat.R
+++ |-- src_R/|   |-- 02_string_kalimat.R
@@ -5,8 +5,7 @@ K2 <- " Ia berkata, \"Aku menyayangimu.\""
 K3 <- " Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!"
 K4 <- " Surat keputusan itu bernomor 62/UN.34/19/2023."
 
-# Catatan: Fungsi print() di R secara bawaan akan menampilkan nomor indeks [1] dan tanda petik.
-# Jika ingin hasilnya bersih persis seperti di gambar, gunakan fungsi cat() dengan baris baru (\n):
+# Cetak semua string tanpa indeks [1] dan tanpa spasi tambahan
 cat(K1, "\n")
 cat(K2, "\n")
 cat(K3, "\n")
